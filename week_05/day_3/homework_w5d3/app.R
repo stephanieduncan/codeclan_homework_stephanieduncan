c180b28d41ac18bd9674097ae5a8795ef0e3bcd6
sd1359 <stephanieduncan@outlook.com>
2021-01-08 09:04:23 +0000

    Adding homework.

---
+++ week_05/day_3/homework_w5d3/app.R
@@ -0,0 +1,58 @@
+library(shiny)
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+
+olympics_overall_medals <- CodeClanData::olympics_overall_medals
+
+
+
+ui <- fluidPage(
+    titlePanel(tags$h2("Five Country Medal Comparison")),
+    
+    tabsetPanel(
+        tabPanel("Choose Season",
+                 radioButtons(inputId = "season",
+                              tags$h4("Summer or Winter Olympics?"),
+                              choices = c("Winter", "Summer")
+                 )
+        )
+        , tabPanel("Choose Medal", 
+                   radioButtons(
+                       inputId = "medal",
+                       tags$h4("Choose Medal"),
+                       choices = c("Gold", "Silver", "Bronze"))
+        ), tabPanel("Plot",
+                    plotOutput(outputId = "medal_plot")
+        )
+    )
+)
+
+server <- function(input, output) {
+    
+    output$medal_plot <- renderPlot({
+        colour <- case_when(input$medal == "Gold" ~ "#ffeda0",
+                            input$medal == "Silver" ~ "#636363",
+                            input$medal == "Bronze" ~ "#d95f0e")
+        olympics_overall_medals %>%
+            filter(team %in% c("United States",
+                               "Soviet Union",
+                               "Germany",
+                               "Italy",
+                               "Great Britain")) %>%
+            filter(medal == input$medal) %>%
+            filter(season == input$season) %>%  
+            ggplot() +
+            aes(x = team, y = count) +
+            geom_col(fill = colour) +
+            theme_light() +
+            theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+            labs(
+                x = "Team",
+                y = "Number of Medals"
+            )
+    })
+}
+
+shinyApp(ui = ui, server = server)
