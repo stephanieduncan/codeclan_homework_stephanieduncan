8c8d6201098921eb0431e37cca140d5f8fefe925
sd1359 <stephanieduncan@outlook.com>
2021-01-10 15:27:15 +0000

    Meteorite PDA.

--- week_05/day_5/shiny_games_sales/app.R
+++ week_05/day_5/shiny_games_sales/app.R
@@ -12,22 +12,22 @@ games_sales <- CodeClanData::game_sales
 ui <- fluidPage(
     theme = shinytheme("cerulean"),
     titlePanel("Games"),
-#using fluidRow to make drop down menus fit aesthetically on the page    
+#Using fluidRow to make drop down menus fit aesthetically on the page    
     fluidRow(
-#creating a drop down menu to allow user to show data based on selected genre
+#Creating a drop down menu to allow user to show data based on selected genre
     column(2,
            selectInput("genre",
                        "Select Genre",
                        choices = unique(games_sales$genre))
     ),
-#creating a drop down menu to allow user to show data based on selected platform
+#Creating a drop down menu to allow user to show data based on selected platform
     column(2,
            selectInput("platform",
                        "Select Platform",
                        choices = unique(games_sales$platform))
     ),
 br(),
-#adding an action button to update the table only once the button has been pressed
+#Adding an action button to update the table only once the button has been pressed
     fluidRow(
         column(2,
            actionButton("update", "Show Games") 
@@ -35,9 +35,9 @@ br(),
 )
 ),
     
-#showing a table of the data to be able to search for specific game titles.
+#Creating a table which will display the filtered data 
         column(6, 
-#using DT::dataTableOutput to add a search feature to allow user to search the data by typing into a search box
+#Using DT::dataTableOutput to add a search feature to allow user to search for specific titles by typing into a search box
                DT::dataTableOutput("games_table")
         ),
 column(12,
@@ -45,16 +45,16 @@ column(12,
 )
 )
 server <- function(input, output) {
-    
+    #Applying reactivity to ensure the app can be used interactively
     games_data_filtered <- eventReactive(input$update, {
         games_sales %>% 
             filter(genre == input$genre) %>% 
             filter(platform == input$platform) %>% 
-#removing data irrelevant to user from table output
+#Removing data irrelevant to the user from table output
             select(- sales, -developer, -publisher, -rating)
     })
                                 
-#using DT::dataTableOutput to add a search feature to allow user to search the data by typing into a search box
+#Using DT::dataTableOutput to add a search feature to allow user to search the data by typing into a search box
     output$games_table <- DT::renderDataTable({
         games_data_filtered()
 })
@@ -64,17 +64,17 @@ server <- function(input, output) {
      ggplot(games_data_filtered()) +
             geom_col(aes (x = name, y = user_score, fill = name)) +
         theme_light() +
-            theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+            theme(axis.text.x = element_text(angle = 55, hjust = 1)) +
             labs(
                 title = "User Rating of Games",
-                x = "Game Title",
+                x = "\n Game Title",
                 y = "User Score"
             ) +
             scale_y_continuous(limits = c(0, 10)) +
             theme(legend.position = "none") +
             theme(title = element_text(size = 25)) +
             theme(text = element_text(size = 12)) +
-            theme(axis.text = element_text(size = 11))
+            theme(axis.text = element_text(size = 12))
             
         
     })
